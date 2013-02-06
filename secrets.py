a2743a051f41ef7a3e90d71563e837cf6c16a0e1
Francis Irving <francis@flourish.org>
2013-02-06 22:38:42 +0000

    Tidy

---
+++ secrets.py
@@ -0,0 +1,8 @@
+# ScraperWiki followers/following app
+CONSUMER_KEY = "3CejKAAW7OGqni9lxuU09g"
+CONSUMER_SECRET = "iGI9Um4WRQXyBtWKVI2uU66JTIsw4QN9G9thjCTNo"
+
+CREDS_PRE_VERIFIY = "twitter_pre_session"
+CREDS_VERIFIED = "twitter_verified_session"
+
+
