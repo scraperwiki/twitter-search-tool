bf39e60664eb1824a5a3ae7012aff7d8e8460f05
Francis Irving <francis@flourish.org>
2013-03-13 10:57:41 +0000

    Change secret

--- secrets.py
+++ secrets.py
@@ -1,6 +1,6 @@
 # ScraperWiki followers/following app
-CONSUMER_KEY = "3CejKAAW7OGqni9lxuU09g"
-CONSUMER_SECRET = "iGI9Um4WRQXyBtWKVI2uU66JTIsw4QN9G9thjCTNo"
+CONSUMER_KEY = "2FYdKsOU7Aalvt2k49UCw"
+CONSUMER_SECRET = "S8ly9jNnQGhFokcls2mvCuE8KO0WSD3TM8qOMEbQM0"
 
 CREDS_PRE_VERIFIY = "twitter_pre_session"
 CREDS_VERIFIED = "twitter_verified_session"
