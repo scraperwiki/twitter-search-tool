981b91ec71e6a6d756a1974f36350c823e070736
Francis Irving <francis@flourish.org>
2013-09-23 14:34:40 +0000

    Use new dataset.name command for rename

--- http/code.js
+++ http/code.js
@@ -61,7 +61,7 @@ var scrape_action = function() {
     // Rename the dataset in the user interface
     // (only when they press the main submit button - not for refreshes)
     if ($(this).attr('id') == 'submit') {
-        scraperwiki.tool.rename("Tweets matching '" + q + "'")
+        scraperwiki.dataset.name("Tweets matching '" + q + "'")
     }
 
     // Pass various OAuth bits of data to the Python script that is going to do the work
