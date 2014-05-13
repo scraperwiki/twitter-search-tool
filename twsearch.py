9b41370649999034aa10d99e52f5a9cbbf2d4833
Francis Irving <francis@flourish.org>
2014-05-13 21:18:16 +0000

    When resetting window, new start must be highest id we had so far not current shifted end

--- twsearch.py
+++ twsearch.py
@@ -315,9 +315,10 @@ try:
         if onetime:
             break
 
-    # Update the window, it now starts where we just ended
-    window_start = window_end
+    # Update the window, it now starts from our latest place forward
+    window_start = scraperwiki.sql.select("max(id_str) from tweets")[0]["max(id_str)"]
     window_end = None
+    log("new window! since_id/window_start = {!r} max_id/window_end = {!r}".format(window_start, window_end))
 
     # We've reached as far back as we'll ever get, so we're done forever in one mode
     if not onetime and mode == 'clearing-backlog':
