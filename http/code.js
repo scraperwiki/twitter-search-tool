7c2c8621519cd4f5f1586a799ba30354c6d4d78b
Francis Irving <francis@flourish.org>
2014-05-21 00:06:41 +0000

    Indentation fix

--- http/code.js
+++ http/code.js
@@ -193,7 +193,7 @@ var show_hide_stuff = function(done, rename) {
             if (results['current_status'] == 'clean-slate') {
                 $('#settings-get').show()
             } else if (results['current_status'] == 'invalid-query') {
-        console.log(results)
+                console.log(results)
                 var p = $('<p>').addClass('alert alert-warning').html("<b>That query didn't work!</b> It isn't a valid Twitter search.")
                 $('body').prepend(p)
                 $('#settings-get').show()
